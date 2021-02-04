efd5e91e3093d81b85c7a7114915fc39ef6914d7
EnochStar <1181591005@zust.edu.cn>
2021-02-04 23:22:57 +0800

    API网关搭建

---
+++ guns-api/src/main/java/com/stylefeng/guns/api/user/UserAPI.java
@@ -0,0 +1,12 @@
+package com.stylefeng.guns.api.user;
+
+/**
+ * @author EnochStar
+ * @title: UserAPI
+ * @projectName guns-parent
+ * @description: TODO
+ * @date 2021/2/4 23:00
+ */
+public interface UserAPI {
+    boolean login(String username,String password);
+}
---
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/user/impl/UserImpl.java
@@ -0,0 +1,21 @@
+package com.stylefeng.guns.rest.modular.user.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.stylefeng.guns.api.user.UserAPI;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author EnochStar
+ * @title: UserImpl
+ * @projectName guns-parent
+ * @description: TODO
+ * @date 2021/2/4 23:00
+ */
+@Component
+@Service(interfaceClass = UserAPI.class)
+public class UserImpl implements UserAPI {
+    @Override
+    public boolean login(String username, String password) {
+        return false;
+    }
+}
