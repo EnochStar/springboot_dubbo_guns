ad6bce3cdc4d6441b5ff14d200759596699c11f7
EnochStar <1181591005@zust.edu.cn>
2021-02-09 21:13:32 +0800

    影片模块 自动代码生成

---
+++ guns-film/src/main/java/com/stylefeng/guns/rest/common/persistence/dao/MoocFilmTMapper.java
@@ -0,0 +1,16 @@
+package com.stylefeng.guns.rest.common.persistence.dao;
+
+import com.stylefeng.guns.rest.common.persistence.model.MoocFilmT;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 影片主表 Mapper 接口
+ * </p>
+ *
+ * @author EnochStar
+ * @since 2021-02-09
+ */
+public interface MoocFilmTMapper extends BaseMapper<MoocFilmT> {
+
+}
