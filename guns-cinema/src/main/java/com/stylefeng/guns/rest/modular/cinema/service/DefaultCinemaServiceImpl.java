a2dfefd353f778209bf41df410c6ec65e807f841
EnochStar <1181591005@zust.edu.cn>
2021-02-19 20:47:52 +0800

    影院模块 业务实现

--- guns-cinema/src/main/java/com/stylefeng/guns/rest/modular/cinema/service/DefaultCinemaServiceImpl.java
+++ guns-cinema/src/main/java/com/stylefeng/guns/rest/modular/cinema/service/DefaultCinemaServiceImpl.java
@@ -1,13 +1,19 @@
 package com.stylefeng.guns.rest.modular.cinema.service;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.baomidou.mybatisplus.plugins.Page;
 import com.stylefeng.guns.api.cinema.CinemaServiceAPI;
 import com.stylefeng.guns.api.cinema.vo.*;
 import com.stylefeng.guns.rest.common.persistence.dao.*;
+import com.stylefeng.guns.rest.common.persistence.model.MoocAreaDictT;
+import com.stylefeng.guns.rest.common.persistence.model.MoocBrandDictT;
+import com.stylefeng.guns.rest.common.persistence.model.MoocCinemaT;
+import com.stylefeng.guns.rest.common.persistence.model.MoocHallDictT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -41,28 +47,145 @@ public class DefaultCinemaServiceImpl implements CinemaServiceAPI {
 
     @Override
     public Page<CinemaVO> getCinemas(CinemaQueryVO cinemaQueryVO) {
+        List<CinemaVO> cinemas = new ArrayList<>();
 
-        return null;
+        Page<MoocCinemaT> page = new Page<>(cinemaQueryVO.getNowPage(),cinemaQueryVO.getPageSize());
+        EntityWrapper<MoocCinemaT> entityWrapper = new EntityWrapper<>();
+        if (cinemaQueryVO.getBrandId() != 99) {
+            entityWrapper.eq("brand_id",cinemaQueryVO.getBrandId());
+        }
+        if (cinemaQueryVO.getDistrictId() != 99) {
+            entityWrapper.eq("area_id",cinemaQueryVO.getDistrictId());
+        }
+        if (cinemaQueryVO.getHallType() != 99) {
+            entityWrapper.like("hall_ids","%#"+cinemaQueryVO.getHallType()+"#%");
+        }
+        List<MoocCinemaT> moocCinemaTS = moocCinemaTMapper.selectPage(page, entityWrapper);
+        for(MoocCinemaT moocCinemaT : moocCinemaTS){
+            CinemaVO cinemaVO = new CinemaVO();
+
+            cinemaVO.setUuid(moocCinemaT.getUuid()+"");
+            cinemaVO.setMinimumPrice(moocCinemaT.getMinimumPrice()+"");
+            cinemaVO.setCinemaName(moocCinemaT.getCinemaName());
+            cinemaVO.setAddress(moocCinemaT.getCinemaAddress());
+
+            cinemas.add(cinemaVO);
+        }
+        long counts = moocCinemaTMapper.selectCount(entityWrapper);
+        Page<CinemaVO> result = new Page<>();
+        result.setRecords(cinemas);
+        result.setTotal(counts);
+        result.setSize(cinemaQueryVO.getPageSize());
+        return result;
     }
 
     @Override
     public List<BrandVO> getBrands(int brandId) {
-        return null;
+        boolean flag = false;
+        MoocBrandDictT moocBrandDictT = moocBrandDictTMapper.selectById(brandId);
+        if (brandId == 99 || moocBrandDictT == null)
+            flag = true;
+        List<BrandVO> brands = new ArrayList<>();
+        List<MoocBrandDictT> moocBrandDictTS = moocBrandDictTMapper.selectList(null);
+        for(MoocBrandDictT moocBrand : moocBrandDictTS) {
+            BrandVO brandVO = new BrandVO();
+            brandVO.setBrandId(moocBrand.getUuid() + "");
+            brandVO.setBrandName(moocBrand.getShowName());
+            if (flag) {
+                if (moocBrand.getUuid() == 99)
+                    brandVO.setActive(true);
+            }else{
+                if (brandId == moocBrand.getUuid()) {
+                    brandVO.setActive(true);
+                }
+            }
+            brands.add(brandVO);
+        }
+        return brands;
     }
 
     @Override
     public List<AreaVO> getAreas(int areaId) {
-        return null;
+        boolean flag = false;
+        List<AreaVO> areaVOS = new ArrayList<>();
+        // 判断brandId是否存在
+        MoocAreaDictT moocAreaDictT = moocAreaDictTMapper.selectById(areaId);
+        // 判断brandId 是否等于 99
+        if(areaId == 99 || moocAreaDictT==null || moocAreaDictT.getUuid() == null){
+            flag = true;
+        }
+        // 查询所有列表
+        List<MoocAreaDictT> moocAreaDictTS = moocAreaDictTMapper.selectList(null);
+        // 判断flag如果为true，则将99置为isActive
+        for(MoocAreaDictT area : moocAreaDictTS){
+            AreaVO areaVO = new AreaVO();
+            areaVO.setAreaName(area.getShowName());
+            areaVO.setAreaId(area.getUuid()+"");
+            // 如果flag为true，则需要99，如为false，则匹配上的内容为active
+            if(flag){
+                if(area.getUuid() == 99){
+                    areaVO.setActive(true);
+                }
+            }else{
+                if(area.getUuid() == areaId){
+                    areaVO.setActive(true);
+                }
+            }
+
+            areaVOS.add(areaVO);
+        }
+
+        return areaVOS;
     }
 
     @Override
     public List<HallTypeVO> getHallTypes(int hallType) {
-        return null;
+        boolean flag = false;
+        List<HallTypeVO> hallTypeVOS = new ArrayList<>();
+        // 判断brandId是否存在
+        MoocHallDictT moocHallDictT = moocHallDictTMapper.selectById(hallType);
+        // 判断brandId 是否等于 99
+        if(hallType == 99 || moocHallDictT==null || moocHallDictT.getUuid() == null){
+            flag = true;
+        }
+        // 查询所有列表
+        List<MoocHallDictT> moocHallDictTS = moocHallDictTMapper.selectList(null);
+        // 判断flag如果为true，则将99置为isActive
+        for(MoocHallDictT hall : moocHallDictTS){
+            HallTypeVO hallTypeVO = new HallTypeVO();
+            hallTypeVO.setHalltypeName(hall.getShowName());
+            hallTypeVO.setHalltypeId(hall.getUuid()+"");
+            // 如果flag为true，则需要99，如为false，则匹配上的内容为active
+            if(flag){
+                if(hall.getUuid() == 99){
+                    hallTypeVO.setActive(true);
+                }
+            }else{
+                if(hall.getUuid() == hallType){
+                    hallTypeVO.setActive(true);
+                }
+            }
+
+            hallTypeVOS.add(hallTypeVO);
+        }
+
+        return hallTypeVOS;
     }
 
     @Override
     public CinemaInfoVO getCinemaInfoById(int cinemaId) {
-        return null;
+        MoocCinemaT moocCinemaT = moocCinemaTMapper.selectById(cinemaId);
+        if(moocCinemaT == null){
+            return new CinemaInfoVO();
+        }
+        CinemaInfoVO cinemaInfoVO = new CinemaInfoVO();
+        cinemaInfoVO.setImgUrl(moocCinemaT.getImgAddress());
+        cinemaInfoVO.setCinemaPhone(moocCinemaT.getCinemaPhone());
+        cinemaInfoVO.setCinemaName(moocCinemaT.getCinemaName());
+        cinemaInfoVO.setCinemaId(moocCinemaT.getUuid()+"");
+        cinemaInfoVO.setCinemaId(moocCinemaT.getCinemaAddress());
+
+        return cinemaInfoVO;
     }
 
     @Override
